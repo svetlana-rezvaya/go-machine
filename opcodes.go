f6b9eb12abe11a487a3f7b35223cdc1b69d59e99
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-03-22 20:42:48 +0300

    Add getOpcodeParameterCount function

--- opcodes.go
+++ opcodes.go
@@ -2,3 +2,11 @@ package main
 
 const loadConstantOpcode = 1
 const loadMemoryOpcode = 2
+
+func getOpcodeParameterCount(opcode int) int {
+	if opcode == loadConstantOpcode || opcode == loadMemoryOpcode {
+		return 2
+	}
+
+	return 0
+}

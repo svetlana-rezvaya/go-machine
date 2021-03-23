5c57d3dbeb93008e1ca8710419579f3ebf9a7ccd
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-03-23 20:08:38 +0300

    Add isOpcodeKnown function

--- fetch_instruction.go
+++ fetch_instruction.go
@@ -5,6 +5,13 @@ import "fmt"
 func fetchInstruction(computer machine) (instruction, error) {
 	ipRegister := computer.registers[computer.ipRegisterIndex]
 	instructionKind := computer.memory[ipRegister]
+	if !isOpcodeKnown(instructionKind) {
+		return instruction{}, fmt.Errorf(
+			"unknown instruction 0x%x at address 0x%x",
+			instructionKind,
+			ipRegister,
+		)
+	}
 
 	fetchedInstruction := instruction{kind: instructionKind}
 	if instructionKind == loadConstantOpcode {
@@ -17,12 +24,6 @@ func fetchInstruction(computer machine) (instruction, error) {
 		registerIndex := computer.memory[ipRegister+2]
 
 		fetchedInstruction.parameters = []int{memoryIndex, registerIndex}
-	} else {
-		return instruction{}, fmt.Errorf(
-			"unknown instruction 0x%x at address 0x%x",
-			instructionKind,
-			ipRegister,
-		)
 	}
 
 	return fetchedInstruction, nil
--- opcodes.go
+++ opcodes.go
@@ -3,6 +3,10 @@ package main
 const loadConstantOpcode = 1
 const loadMemoryOpcode = 2
 
+func isOpcodeKnown(opcode int) bool {
+	return opcode == loadConstantOpcode || opcode == loadMemoryOpcode
+}
+
 func getOpcodeParameterCount(opcode int) int {
 	if opcode == loadConstantOpcode || opcode == loadMemoryOpcode {
 		return 2

f810e2d112c83855b3c1a3bb0917fbb128548002
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-03-31 21:18:39 +0300

    Add a failure test for fetchInstruction function

--- fetch_instruction_test.go
+++ fetch_instruction_test.go
@@ -25,3 +25,22 @@ func Test_fetchInstruction_success(test *testing.T) {
 		test.Fail()
 	}
 }
+
+func Test_fetchInstruction_error(test *testing.T) {
+	machineInstance := machine{
+		memory:          []int{12, 19, 14, 18, 16, 17, 15, 13},
+		registers:       []int{2, 9, 4, 8, 6, 1, 5, 3},
+		ipRegisterIndex: 2,
+	}
+	instructionInstance, err := fetchInstruction(machineInstance)
+
+	wantedInstructionInstance := instruction{}
+	if !reflect.DeepEqual(instructionInstance, wantedInstructionInstance) {
+		test.Fail()
+	}
+
+	wantedErrStr := "unknown instruction 0x10 at address 0x4"
+	if err == nil || err.Error() != wantedErrStr {
+		test.Fail()
+	}
+}

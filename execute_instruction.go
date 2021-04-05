277c572102f40f8e5dbbe0edac5947d28ff9762d
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-04-05 21:41:54 +0300

    Implement executing of loadConstant instruction

--- execute_instruction.go
+++ execute_instruction.go
@@ -2,7 +2,10 @@ package main
 
 func executeInstruction(computer machine, instructionInstance instruction) {
 	if instructionInstance.kind == loadConstantOpcode {
+		constant := instructionInstance.parameters[0]
+		registerIndex := instructionInstance.parameters[1]
 
+		computer.registers[registerIndex] = constant
 	} else if instructionInstance.kind == loadMemoryOpcode {
 
 	}
---
+++ execute_instruction_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_executeInstruction_withLoadConstantOpcode(test *testing.T) {
+	machineInstance := machine{
+		memory:          []int{12, 1, 5, 3, 2, 17, 15, 13},
+		registers:       []int{2, 9, 4, 8, 6, 1, 5, 3},
+		ipRegisterIndex: 5,
+	}
+	instructionInstance, err := fetchInstruction(machineInstance)
+	if err != nil {
+		test.FailNow()
+	}
+
+	executeInstruction(machineInstance, instructionInstance)
+
+	wantedMachineInstance := machine{
+		memory:          []int{12, 1, 5, 3, 2, 17, 15, 13},
+		registers:       []int{2, 9, 4, 5, 6, 1, 5, 3},
+		ipRegisterIndex: 5,
+	}
+	if !reflect.DeepEqual(machineInstance, wantedMachineInstance) {
+		test.Fail()
+	}
+}

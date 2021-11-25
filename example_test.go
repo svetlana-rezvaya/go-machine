9d47134b36fa901e8c0bdbb73999a893b9082c4f
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-11-25 23:04:44 +0300

    Add the example

--- example_test.go
+++ example_test.go
@@ -1,22 +1,4 @@
-# go-machine
-
-[![GoDoc](https://godoc.org/github.com/svetlana-rezvaya/go-machine?status.svg)](https://godoc.org/github.com/svetlana-rezvaya/go-machine)
-[![Go Report Card](https://goreportcard.com/badge/github.com/svetlana-rezvaya/go-machine)](https://goreportcard.com/report/github.com/svetlana-rezvaya/go-machine)
-[![Build Status](https://app.travis-ci.com/svetlana-rezvaya/go-machine.svg?branch=master)](https://app.travis-ci.com/svetlana-rezvaya/go-machine)
-[![codecov](https://codecov.io/gh/svetlana-rezvaya/go-machine/branch/master/graph/badge.svg)](https://codecov.io/gh/svetlana-rezvaya/go-machine)
-
-The library implementing a simple virtual machine.
-
-## Installation
-
-```
-$ go get github.com/svetlana-rezvaya/go-machine
-```
-
-## Usage Example
-
-```go
-package main
+package machine_test
 
 import (
 	"fmt"
@@ -100,7 +82,7 @@ func isPrime(number int) (bool, error) {
 	return isSuccessful, nil
 }
 
-func main() {
+func Example() {
 	for number := 0; number < 100; number++ {
 		isSuccessful, err := isPrime(number)
 		if err != nil {
@@ -139,10 +121,3 @@ func main() {
 	// number 89 is prime
 	// number 97 is prime
 }
-```
-
-## License
-
-The MIT License (MIT)
-
-Copyright &copy; 2021 svetlana-rezvaya
